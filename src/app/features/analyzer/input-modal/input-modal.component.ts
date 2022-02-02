fcff031943d204b7ae935b81581095fb7bd9160e
Pablo Gomez Restrepo <pablo.gomezr@perficient.com>
2022-02-02 08:45:17 -0500

    agregando la entrada del automata al sistema

--- src/app/features/analyzer/input-modal/input-modal.component.ts
+++ src/app/features/analyzer/input-modal/input-modal.component.ts
@@ -1,6 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
+
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
+import { IState } from '../interfaces/IStae';
+import { ITransision } from '../interfaces/ITransision';
 
 // export interface DialogData {
 //   animal: string;
@@ -12,16 +16,23 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   templateUrl: './input-modal.component.html',
   styleUrls: ['./input-modal.component.scss']
 })
-export class InputModalComponent implements OnInit{
+export class InputModalComponent implements OnInit {
 
   isLinear = true;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   lastFormGroup: FormGroup;
+  // enableAddTransision: boolean;
 
-  public states!: string[];
-  public currentState!: string;
+  public states!: IState[];
+  public inputs: string[] = [];
+  public transisions: ITransision[];
+  public currentState: IState | undefined;
   public stateIndex: number = 0;
+  public inputIndex: number = 0;
+  public automata: any;
+
+  public currentTransision!: ITransision | undefined;
 
   constructor(
     public dialogRef: MatDialogRef<InputModalComponent>,
@@ -29,29 +40,106 @@ export class InputModalComponent implements OnInit{
     // ,@Inject(MAT_DIALOG_DATA) public data: DialogData
   ) {
 
-    this.states = [ 
+    this.states = [
     ]
+    this.transisions = []
+    // this.enableAddTransision =  true
+
+    this.automata = {}
 
 
   }
+
+  selectedStatus: Array<string>;
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
+      firstCtrl: [' ', Validators.required],
+      statusControl: [true],
     });
     this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
+      name: ['', Validators.required]
     });
     this.lastFormGroup = this._formBuilder.group({
-      lastCtrl: ['', Validators.required]
+      transision: ['', Validators.required]
     });
   }
 
-  public addState(state: string): void {
-    this.states.push(state);
-    this.currentState = ''
+  public resetstates() {
+    this.states = [];
+    this.firstFormGroup.controls['firstCtrl'].setValue(' ');
+    this.firstFormGroup.controls['statusControl'].setValue(false);
   }
-  
+
+  public addState(): void {
+    let name = this.firstFormGroup.controls['firstCtrl'].value;
+    let acceptance = this.firstFormGroup.controls['statusControl'].value;
+    let state = {
+      name: name,
+      acceptance: acceptance
+    }
+    this.states.push(state);
+    this.firstFormGroup.controls['firstCtrl'].setValue(' ');
+    this.firstFormGroup.controls['statusControl'].setValue(false);
+  }
+
+  public addInput(): void {
+    if (this.states.length > 0) {
+      this.currentState = this.states[0];
+    }
+    let name = this.secondFormGroup.controls['name'].value;
+    this.inputs.push(name);
+    this.secondFormGroup.controls['name'].setValue(' ');
+  }
+
+  public addTransision(): void {
+    // PUSH
+
+    if (this.currentState && this.currentState.name) {
+
+      if (this.currentTransision && this.currentTransision.state === this.currentState.name) {
+        this.currentTransision.inputs.push({
+          to: this.states[this.stateIndex].name, // estado
+          value: this.lastFormGroup.controls['transision'].value //entrada
+        })
+
+      } else {
+        // ENTRA LA PRIMER VEZ
+        this.currentTransision = {
+          state: '',
+          inputs: []
+        }
+        this.currentTransision.state = this.currentState.name;
+        this.currentTransision.inputs.push({
+          to: this.states[this.stateIndex].name, // estado
+          value: this.lastFormGroup.controls['transision'].value //entrada
+        })
+      }
+
+      if (this.inputs.length - 1 > this.inputIndex) {
+        this.inputIndex++;
+      } else {
+        this.inputIndex = 0;
+        this.stateIndex++;
+
+        if (this.currentTransision) {
+          this.transisions.push(this.currentTransision);
+          this.currentTransision = undefined;
+        }
+      }
+      this.currentState = this.states[this.stateIndex];
+      // console.log('transitions', this.transisions)
+    }
+  }
+
+  getAutomata(): any {
+    return {
+      states: this.states,
+      inputs: this.inputs,
+      trancisions: this.transisions
+    }
+  }
+
   /**
    * name
    */
